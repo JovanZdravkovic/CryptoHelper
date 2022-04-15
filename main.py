77741058f0d8acdc0765fcf577a85705a59d1826
Zdravkson <jovan.zdravkovic882@gmail.com>
2022-04-15 15:33:21 +0200

    Added the notification push

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import re
 from datetime import date, timedelta
 import pandas as pd
 import time
+from gi.repository import Notify
 
 startdate = str(date.today() - timedelta(days = 1))
 enddate = str(date.today())
 
+SELL_VALUE = 50000.0
+BUY_VALUE = 35000.0
+
+Notify.init("Test Notifier")
+
 while True:
     btc = fq.get_crypto_data('BTC/USDT', startdate, enddate)
-    print(btc.loc[enddate, "close"])
+    btcvalue = btc.loc[enddate, "close"]
+    print(btcvalue)
+    if btcvalue >= SELL_VALUE:
+        Notify.Notification.new("Bitcoin value is high! SELL!").show()
+    if btcvalue <= BUY_VALUE:
+        Notify.Notification.new("Bitcoin value is low! BUY!").show()
     time.sleep(5)
